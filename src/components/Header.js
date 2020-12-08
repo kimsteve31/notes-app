a775b5a4f1345462417bc1f1f641ae337354da22
Steve Kim <kimsteve31@gmail.com>
2020-12-08 11:54:29 -0500

    Initial commit to notes-app

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <div>
+      <i class="fas fa-bars"></i>
+      <h3 className="notz-header">Notz</h3>
+    </div>
+  )
+}
+
+export default Header;
